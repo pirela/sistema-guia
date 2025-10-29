664a5ff250f232244d3f14fcb8e0c3adf86cc544
Jose Pirela <jjpirela93@gmail.com>
2025-10-29 12:29:40 -0500

    fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,8 @@
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+//const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClientComponentClient()
 //export const supabase = createClient(supabaseUrl, supabaseAnonKey)
